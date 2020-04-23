befbfd94e154b1abbfa2b606d395ea80977a7b7e
Michael <michael.p.vaden@gmail.com>
2020-04-22 20:17:40 -0700

    solved reverse

---
+++ difficult_recursion/reverse.js
@@ -0,0 +1,18 @@
+/*
+**  Write a recursive function called reverse which accepts a string and returns a new string in reverse
+
+**  Sample Input:
+**  reverse('awesome') // 'emosewa'
+**   reverse('rithmschool') // 'loohcsmhtir'
+*/
+
+function reverse(str) {
+        if(str.length === 1) {return str[0];}
+        return str[str.length - 1] + reverse(str.slice(0, (str.length - 1)));
+
+}
+
+console.log(
+    reverse('awesome'), // 'emosewa'
+    reverse('rithmschool') // 'loohcsmhtir'
+)
