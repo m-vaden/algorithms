ed967a03af58640d1e2bf866388207a7e260978c
Michael <michael.p.vaden@gmail.com>
2020-05-05 12:18:46 -0700

    completed bubble sort

---
+++ sorting_algorithms/bubble_sort.js
@@ -0,0 +1,28 @@
+/*
+ * A sorting algorithm where the largest values bubble up to the top.
+ * Create a function called bubbleSort that accepts an array.
+ * Start looping from the end of an array towards the beginning with a variable called i.
+ * Start an inner loop with a variable called j from the beginning until i - 1.
+ * If arr[j] is greater than arr[j+1], swap those two values.
+*/
+
+function bubbleSort(arr) {
+    let noSwaps;
+    for(let i = arr.length; i > 0; i--) {
+        noSwaps = true;
+        for(let j = 0; j < i - 1; j++) {
+            if(arr[j] > arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                noSwaps = false;
+            }
+        }
+        if(noSwaps) { break };
+    }
+    return arr;
+}
+
+console.log(
+    bubbleSort([37,45,29,8])
+)
---
+++ sorting_algorithms/sorting.md
@@ -0,0 +1,10 @@
+## Sorting 
+
+    Sorting is the process of rearranging the items in a collection (e.g. an array) so that the items are in some kind of order.
+
+### Examples
+
+    - Sorting numbers from smallest to largest.
+    - Sorting names alphabetically.
+    - Sorting movies based on release year.
+    - Sorting movies based on revenue.
